b7821c76aa0b9e4ca88a37583243419ecaafcf98
Github Sync Pipeline <axytos-ecommerce-plugins@brickmakers.de>
2024-06-07 08:57:57 +0000

    Publish Version 1.6.11-rc, Build 20240607.3

--- metadata.php
+++ metadata.php
@@ -37,7 +37,7 @@ $aModule = array(
         "nl" => "Je moet de factuur betalen zodra je de goederen hebt ontvangen, binnen de betalingstermijn.",
     ],
     "thumbnail"   => "assets/img/logo.png",
-    "version"     => "1.6.10",
+    "version"     => "1.6.11-rc",
     "author"      => "axytos GmbH",
     "url"         => "https://www.axytos.com",
     "email"       => "info@axytos.com",
